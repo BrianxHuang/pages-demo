75c9fca261a0701ccd1e4bd8b8fe23761a9d7e4f
Brian <bbbb840827@gmail.com>
2022-07-29 15:07:04 +0800

    increase phone gesture and size

--- animation.js
+++ animation.js
@@ -1,8 +1,7 @@
 function myMove(dir) {
-
   let status = true;
   clearInterval(moveid);
-  let speed = 10;
+  let speed = ratio*1;
   let vtime = 50;
   let direction;
   moveid = setInterval(move, vtime);  
@@ -59,7 +58,7 @@ function myMove(dir) {
 function eating()
 {
 
-  if(Math.abs(s_pos[0][0]-f_pos[0])<10&&Math.abs(s_pos[0][1]-f_pos[1])<10)
+  if(Math.abs(s_pos[0][0]-f_pos[0])<ratio&&Math.abs(s_pos[0][1]-f_pos[1])<ratio)
   {
     foodrepos();
     count++;
@@ -69,8 +68,8 @@ function eating()
 }
 function foodrepos()
 {
-  f_pos = [Math.random()*(gw_size[0]-30),Math.random()*(gw_size[1]-30)];
-  f_pos = [Math.round(f_pos[0]/10)*10,Math.round(f_pos[1]/10)*10];
+  f_pos = [Math.random()*(gw_sizelimit[0]),Math.random()*(gw_sizelimit[1])];
+  f_pos = [Math.round(f_pos[0]/ratio)*ratio,Math.round(f_pos[1]/ratio)*ratio];
   food.style.left = f_pos[0]+"px";
   food.style.top = f_pos[1]+"px"; 
 }
@@ -85,7 +84,7 @@ function createbody()
   s_pos[count][1]=s_pos[0][1];          
   snakebody[count].style.left = s_pos[count][0] + 'px';  
   snakebody[count].style.top = s_pos[count][1] + 'px'; 
-  foodtobody[count]=0;
+  bodycollison[count]=0;
 }
 function snakepos()
 {      
@@ -106,13 +105,32 @@ function collison()
   {
     if(Math.abs(s_pos[0][0]-s_pos[k][0])==0&&Math.abs(s_pos[0][1]-s_pos[k][1])==0)
     {
-      foodtobody[k]+=1;
-      if (foodtobody[k]>1){
+      bodycollison[k]+=1;      
+      if (bodycollison[k]>1){
         return true;
       }      
     }
   }
   return false;
+}
+function fingerdirection(start,end)
+{
+  let movelength = [end[0]-start[0],end[1]-start[1]];  
+  if (movelength[0]>=0&&(movelength[0]>=Math.abs(movelength[1])))
+  {
+    return "ArrowRight";
+  }else if (movelength[0]<0&&(Math.abs(movelength[0])>=Math.abs(movelength[1])))
+  {
+    return "ArrowLeft";
+  }
+  if (movelength[1]<=0&&(Math.abs(movelength[1])>Math.abs(movelength[0])))
+  {
+    return "ArrowUp";
+  }else if (movelength[1]>0&&((movelength[1])>Math.abs(movelength[0])))
+  {
+    return "ArrowDown";
+  }
+  
 }
 function restart()
 {
@@ -120,6 +138,6 @@ function restart()
   moveid = null;
   snakebody=[head];
   count = 0;
-  foodtobody = [];
+  bodycollison = [];
   statusa = true;
 }
--- script.js
+++ script.js
@@ -2,39 +2,60 @@ var head = document.getElementById("head");
 var body = document.getElementById("body");
 var food = document.getElementById("food");
 var gamewindow = document.getElementById("gamewindow");
-var moveid = null;
-var snakebody=[head];
-var count = 0;
-var foodtobody = [];
-var statusa = true;
-//initial gamewindow, snake, food
-gamewindow.style ="position:relative;border:2px solid Black";
-
-
+var moveid = null; //ID of setInterval
+var snakebody=[head]; //ID list of snake body
+var count = 0; //number of snake body
+var bodycollison = []; //number of snake body collision
+var statusa = true; //check whther ever collision
+var ratio =20; //size ratio; 
+var fingerstart=[];
+var fingerend=[];
 
 //set game windowe size
+gamewindow.style ="position:relative;border:2px solid Black";
 var gw_size = [window.innerWidth,window.innerHeight];
-gw_size = [Math.round(window.innerWidth/10)*10,Math.round(window.innerHeight/10)*10];
-gamewindow.style.width = (gw_size[0]-20)+"px";
-gamewindow.style.height= (gw_size[1]-20)+"px";
-var gw_sizelimit=[gw_size[0]-30,gw_size[1]-30]
+gw_size = [Math.round(window.innerWidth/ratio)*ratio,Math.round(window.innerHeight/ratio)*ratio];
+gamewindow.style.width = (gw_size[0]-40)+"px";
+gamewindow.style.height= (gw_size[1]-40)+"px";
+var gw_sizelimit=[gw_size[0]-40-ratio,gw_size[1]-40-ratio]
 
-//set snake position
-
-var s_pos= [[Math.random()*(gw_size[0]-30),Math.random()*(gw_size[1]-30)]];
-s_pos[0]= [Math.round(s_pos[0][0]/10)*10,Math.round(s_pos[0][1]/10)*10];
-var s_newpos=[];
+//set snake initial position
+var s_pos= [[Math.random()*(gw_sizelimit[0]),Math.random()*(gw_sizelimit[1])]];
+s_pos[0]= [Math.round(s_pos[0][0]/ratio)*ratio,Math.round(s_pos[0][1]/ratio)*ratio];
+var s_newpos=[]; //temp snake new position
 head.style.left = s_pos[0][0] + 'px';
 head.style.top = s_pos[0][1]+ 'px';      
 
-
+//Listen keyborad
 window.addEventListener('keydown',  function(e){
     if (statusa==true){
         clearInterval(moveid);
         myMove(e.key);
     }       
 })
-window.addEventListener("click", function(){ alert("Hello World!"); });
+window.addEventListener('touchstart',  function(e){
+    if (statusa==true){
+        fingerstart[0] = e.changedTouches[0].pageX;
+        fingerstart[1] = e.changedTouches[0].pageY;        
+    }       
+},false)
+
+window.addEventListener('touchmove',  function(e){
+    e.preventDefault();//prevernt screen move
+}, { passive: false })
+
+window.addEventListener('touchend',  function(e){
+    if (statusa==true){
+        fingerend[0] = e.changedTouches[0].pageX;
+        fingerend[1] = e.changedTouches[0].pageY;        
+        clearInterval(moveid);
+        myMove(fingerdirection(fingerstart,fingerend));
+        
+    }       
+},false)
+
+
+//set food initial position
 var f_pos = [];
 foodrepos();
 
